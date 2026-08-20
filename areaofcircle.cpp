a0e6904833ec2791e937c15b79bfa2a2f00d9edb
Jigyashu <Jigyashu.sahu29@gmail.com>
2026-08-20 14:24:39 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float radius , area;
+    
+    cout<<"enter area";
+    cin>>radius;
+    
+    area=3.141*radius*radius;
+    
+    cout<<area;
+    
+
+    return 0;
+}
